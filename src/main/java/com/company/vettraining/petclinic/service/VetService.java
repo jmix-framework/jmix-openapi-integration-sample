23669a540d98752e054616441db26b3a1a4339f8
Gleb Gorelov <glebfox@gmail.com>
2026-06-16 12:03:25 +0400

    Jmix 3.0 migration

--- src/main/java/com/company/vettraining/petclinic/service/VetService.java
+++ src/main/java/com/company/vettraining/petclinic/service/VetService.java
@@ -1,7 +1,7 @@
 package com.company.vettraining.petclinic.service;
 
 import com.company.vettraining.entity.petclinic.Vet;
-import com.company.vettraining.petclinic.api.VetApi;
+import com.company.vettraining.petclinic.api.VetsApi;
 import com.company.vettraining.petclinic.mapper.VetMapper;
 import com.company.vettraining.petclinic.model.VetModel;
 import org.springframework.stereotype.Component;
@@ -11,32 +11,32 @@ import java.util.stream.Collectors;
 
 @Component
 public class VetService {
-    private final VetApi vetApi;
+    private final VetsApi vetsApi;
     private final VetMapper vetMapper;
 
-    public VetService(VetApi vetApi, VetMapper vetMapper) {
-        this.vetApi = vetApi;
+    public VetService(VetsApi vetsApi, VetMapper vetMapper) {
+        this.vetsApi = vetsApi;
         this.vetMapper = vetMapper;
     }
 
     public Vet addVet(Vet vet) {
         VetModel vetModel = vetMapper.toModel(vet);
-        VetModel resultVetModel = vetApi.addVet(vetModel);
+        VetModel resultVetModel = vetsApi.addVet(vetModel);
         return vetMapper.toEntity(resultVetModel);
     }
 
     public Vet deleteVet(Integer vetId) {
-        VetModel vetModel = vetApi.deleteVet(vetId);
+        VetModel vetModel = vetsApi.deleteVet(vetId);
         return vetMapper.toEntity(vetModel);
     }
 
     public Vet getVet(Integer vetId) {
-        VetModel vetModel = vetApi.getVet(vetId);
+        VetModel vetModel = vetsApi.getVet(vetId);
         return vetMapper.toEntity(vetModel);
     }
 
     public List<Vet> listVets() {
-        List<VetModel> vetModels = vetApi.listVets();
+        List<VetModel> vetModels = vetsApi.listVets();
         List<Vet> vets = vetModels.stream()
                 .map(vetMapper::toEntity)
                 .collect(Collectors.toList());
@@ -46,7 +46,7 @@ public class VetService {
     // tag::update[]
     public Vet updateVet(Integer vetId, Vet vet) {
         VetModel vetModel = vetMapper.toModel(vet);
-        VetModel resultVetModel = vetApi.updateVet(vetId, vetModel);
+        VetModel resultVetModel = vetsApi.updateVet(vetId, vetModel);
         return resultVetModel != null ?
                 vetMapper.toEntity(resultVetModel) :
                 getVet(vetId);
